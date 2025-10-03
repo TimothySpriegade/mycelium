20c0a798b53495e6de1dc724b44b27cff3593f52
Timothy Spriegade <timothyspriegade@outlook.de>
2025-10-03 13:33:47 +0200

    added a simpel repl system that prints out tokens for now

--- main.go
+++ main.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"mycelium/repl"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s! This is the mycelium programming langauge!\n", user.Username)
+	fmt.Printf("Have fun trying some commands\n")
+	repl.Start(os.Stdin, os.Stdout)
+}
